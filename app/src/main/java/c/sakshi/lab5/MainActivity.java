01ef261128af1fdafecf3745e6b2fb71bf87c141
Andy <aking27@wisc.edu>
2020-02-28 15:34:31 -0600

    Username and password working

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,18 +1,39 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
     }
 
     public void login(View view){
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        Toast toast = Toast.makeText(this, user + " " + pass, Toast.LENGTH_SHORT);
+        toast.show();
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
 
     }
 }
